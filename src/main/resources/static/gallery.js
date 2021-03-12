bfeba8db764a413670a6d1e172ec5686a2cd39b3
samuel <samuel@sams-laptop>
2021-03-12 20:21:01 +0000

    cropper

--- src/main/resources/static/gallery.js
+++ src/main/resources/static/gallery.js
@@ -1,11 +1,27 @@
-jQuery(documernt).ready(() => {
+jQuery(document).ready(() => {
+	var cropBoxData;
+      var canvasData;
+      var cropper;
 	jQuery("#file").on("change", (e) => {
 	    var files = e.target.files;
 	    if (files && files.length > 0) {
 	      var file = files[0];
 	      var url = URL.createObjectURL(file);
-	      jQuery("#upload").attr("src", url);
-	      new Cropper(jQuery("#upload")[0]);
+	      jQuery("#image-upload").attr("src", url);
+		  jQuery("#modal").modal("show");
+	      cropper = new Cropper(jQuery("#image-upload")[0],{
+          autoCropArea: 0.5,
+          ready: function () {
+            //Should set crop box data first here
+            cropper.setCropBoxData(cropBoxData).setCanvasData(canvasData);
+          }
+        });
 	    }
 	});
+	jQuery('#modal').on('hidden.bs.modal', function () {
+        cropBoxData = cropper.getCropBoxData();
+        canvasData = cropper.getCanvasData();
+        cropper.destroy();
+      });
 });
+
